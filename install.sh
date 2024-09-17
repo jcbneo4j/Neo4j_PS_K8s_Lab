4249082fa3a1205c546fa5b8baf01929f4a1cf7b
jcbneo4j <106252824+jcbneo4j@users.noreply.github.com>
2024-09-17 10:04:53 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 
-#curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
 k3d cluster create --config cluster-config.yml
