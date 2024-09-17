cb05011d6dbf11831742f5f03a9442be17db5ce6
jcbneo4j <106252824+jcbneo4j@users.noreply.github.com>
2024-09-17 14:17:53 +0000

    new file:   deploy.sh 	modified:   install.sh

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+helm install server-1 neo4j/neo4j --namespace neo4j -f values.yaml
+helm install server-2 neo4j/neo4j --namespace neo4j -f values.yaml
+helm install server-3 neo4j/neo4j --namespace neo4j -f values.yaml
--- install.sh
+++ install.sh
@@ -2,3 +2,10 @@
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
 k3d cluster create --config cluster-config.yml
+
+
+helm repo add neo4j https://helm.neo4j.com/neo4j
+helm repo update
+
+kubectl create namespace neo4j
+kubectl config set-context --current --namespace=neo4j
