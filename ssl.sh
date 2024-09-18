02328b7395888610e7941aaa0ab38e8313d0e73d
jcbneo4j <106252824+jcbneo4j@users.noreply.github.com>
2024-09-18 15:50:20 +0000

    .

--- ssl.sh
+++ ssl.sh
@@ -1,3 +1,5 @@
+#generate the cert and private key
 openssl req -newkey rsa:2048 -nodes -keyout private.key -x509 -days 365 -out public.crt -subj "/C=GB/ST=London/L=London/O=Neo4j/OU=IT Department"
 
+#add to a kubernetes secret named neo4j-tls
 kubectl create secret tls neo4j-tls --cert=public.crt --key=private.key
