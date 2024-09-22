216a19241e67e78d6c7d3c23e8a00793366c5a83
jcbneo4j <106252824+jcbneo4j@users.noreply.github.com>
2024-09-22 14:34:49 -0400

    Update clean_all.sh

--- clean_all.sh
+++ clean_all.sh
@@ -1,1 +1,10 @@
 k3d cluster delete neo4jcluster
+
+./install.sh
+
+helm repo add neo4j https://helm.neo4j.com/neo4j
+helm repo update
+
+kubectl create namespace neo4j
+kubectl config set-context --current --namespace=neo4j
+
